ec51f5f97ea3ab86179d187280fc6c01754ac93a
SoftCore <soft0core1@gmail.com>
2025-08-31 11:43:30 +0100

    page reload bug fix

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ import routes from "./routers/index.js";
 import mongoose from "mongoose";
 import { createClient } from "redis";
 import { RedisStore } from "connect-redis";
-import { COOKIE_NAME, MONGODB_CONNECTION_URL, PORT, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME, SESSION_SECRET, } from "./utils/configs.js";
-import { join } from "path";
+import {
+  COOKIE_NAME,
+  MONGODB_CONNECTION_URL,
+  PORT,
+  REDIS_HOST,
+  REDIS_PASSWORD,
+  REDIS_PORT,
+  REDIS_USERNAME,
+  SESSION_SECRET,
+} from "./utils/configs.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const distPath = join(__dirname, "dist", "index.html")
 const app = express();
 const redisClient = createClient({
-    username: REDIS_USERNAME,
-    password: REDIS_PASSWORD,
-    socket: {
-        host: REDIS_HOST,
-        port: REDIS_PORT,
-    },
+  username: REDIS_USERNAME,
+  password: REDIS_PASSWORD,
+  socket: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  },
 });
 /* const redisClient = createClient({
   url: "redis://localhost:6379",
@@ -22,37 +36,44 @@ const redisClient = createClient({
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 await redisClient.connect();
 const redisStore = new RedisStore({
-    client: redisClient,
+  client: redisClient,
 });
 mongoose
-    .connect(`${MONGODB_CONNECTION_URL}`)
-    .then(() => console.log("Database connection established"))
-    .catch((err) => console.log(err));
+  .connect(`${MONGODB_CONNECTION_URL}`)
+  .then(() => console.log("Database connection established"))
+  .catch((err) => console.log(err));
 app.set("trust proxy", 1);
 app.use(
-/* cors({ //CORS configuration when you want to expose api to any origin
+  /* cors({ //CORS configuration when you want to expose api to any origin
   credentials: true,
   origin: true,
 }), */
-express.static('./dist'), json(), session({
+  express.static("./dist"),
+  json(),
+  session({
     store: redisStore,
     secret: SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
     name: COOKIE_NAME,
     cookie: {
-        maxAge: 60000 * 60,
-        httpOnly: false,
-        secure: false,
+      maxAge: 60000 * 60,
+      httpOnly: false,
+      secure: false,
     },
     rolling: true,
-}), routes);
+  }),
+  routes
+);
 app.get("/signin", (req, res) => {
-    res.redirect("/");
+  res.redirect("/");
 });
-app.get("/signup", (req, res) => {
-    res.redirect("/");
+
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(distPath);
 });
+
 app.listen(PORT, () => {
-    console.log(`This application is running on Port:${PORT}`);
+  console.log(`This application is running on Port:${PORT}`);
 });
